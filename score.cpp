421cd0c66b2975a83987d73ad41c13069b64adbb
geniuszhaoyi <genius.zhaoyi@gmail.com>
2014-07-24 10:32:26 +0800

    test
    prepare to merge

--- score.cpp
+++ score.cpp
@@ -1,5 +1,3 @@
-#include <math.h>
-
 #include "main.h"
 
 const double M[]={0,0,0.014,0,0,0.395,0.317,0,0.389,0.079,0.445,0.508,0.613,0.851,0.732,0.828,0.615,0.804,0.685,0.583};
@@ -75,7 +73,7 @@ double subscore(int ini,int j,int *Nph,int type){
         smm=25.0;
     }else{
         if(nmm<=NUM_NO){
-            smm=20.0*smm/(double)nmm/(double)nmm/(4.0*d0/19.0/(double)nmm+1);
+            smm=2.0*smm/(double)nmm/(double)nmm/(4.0*d0/19.0/(double)nmm+1);
             if(type==1) in_site[ini].ot.push_back(j);
         }else{
             smm=0.0;
@@ -86,7 +84,8 @@ double subscore(int ini,int j,int *Nph,int type){
     return smm;
 }
 
-return_struct score(int ii,int *pini,int type){
+return_struct score(int ii,int *pini,int type,double r1){
+    double r2=1.0-r1;
     int ini=*pini;
     int Sgc=0,S20=0;
     int Nph=0;
@@ -95,48 +94,65 @@ return_struct score(int ii,int *pini,int type){
     int i;
     return_struct rs;
 
+    in_site[ini]=psb_site[ii];
+    in_site[ini].ot.clear();
+
     if(check_rfc(ini)==0){
         rs.dou[0]=-1.0;
         rs.dou[1]=0.0;
         rs.dou[2]=0.0;
+        dc_put(0,ini);
+        return rs;
     }
 
-    in_site[ini]=psb_site[ii];
-    in_site[ini].ot.clear();
+    cJSON *cache=dc_get(in_site[ini].chromosome,in_site[ini].index,in_site[ini].strand);
+    if(cache!=NULL){
+        in_site[ini].score=cJSON_GetObjectItem(cache,"score")->valuedouble;
+        in_site[ini].Sspe=cJSON_GetObjectItem(cache,"Sspe")->valuedouble;
+        in_site[ini].Seff=cJSON_GetObjectItem(cache,"Seff")->valuedouble;
+        in_site[ini].count=cJSON_GetObjectItem(cache,"count")->valuedouble;
+        in_site[ini].otj=cJSON_GetObjectItem(cache,"offtarget");
+        (*pini)++;
+        rs.dou[0]=in_site[ini].score;
+        rs.dou[1]=in_site[ini].Sspe;
+        rs.dou[2]=in_site[ini].Seff;
+        return rs;
+    }
 
     for(i=0;i<LEN;i++) if(in_site[ini].nt[i]=='C' || in_site[ini].nt[i]=='G') gc++;
-    if((double)gc/(double)LEN<0.4 || (double)gc/(double)LEN>0.8) Sgc=5;
-    if(in_site[ini].nt[19]!='G') S20=2;
+    if((double)gc/(double)LEN<0.4 || (double)gc/(double)LEN>0.8) Sgc=65;
+    else if((double)gc/(double)LEN>0.5 && (double)gc/(double)LEN<0.7) Sgc=0;
+    else Sgc=35;
+    if(in_site[ini].nt[19]!='G') S20=35;
 
     for(int j=0;j<pi;j++) if(in_site[ini].index!=psb_site[j].index){
         double smm=subscore(ini,j,&Nph,1);
         sum+=smm;
     }
-    rs.dou[1]=93.0-sum;
-    rs.dou[2]=7.0-Sgc-S20;
+    //sum=sigma+S1
     if(type==1 && Nph>3){
-        in_site[ini].score=0.0;
-        in_site[ini].Sspe=rs.dou[1];
-        in_site[ini].Seff=rs.dou[2];
+        in_site[ini].Sspe=rs.dou[1]=max(r1*(100-sum),0.0);
+        in_site[ini].Seff=rs.dou[2]=r2*(100-Sgc-S20);
+        in_site[ini].score=in_site[ini].Sspe+in_site[ini].Seff;
         in_site[ini].count=in_site[ini].ot.size();
         (*pini)++;
         rs.dou[0]=0.0;
     }else if(type==1){
-        sum=100-sum-Sgc-S20;
-        in_site[ini].score=sum;
-        in_site[ini].Sspe=rs.dou[1];
-        in_site[ini].Seff=rs.dou[2];
+        in_site[ini].Sspe=rs.dou[1]=max(r1*(100-sum),0.0);
+        in_site[ini].Seff=rs.dou[2]=r2*(100-Sgc-S20);
+        in_site[ini].score=rs.dou[0]=in_site[ini].Sspe+in_site[ini].Seff;
         in_site[ini].count=in_site[ini].ot.size();
         (*pini)++;
-        rs.dou[0]=sum;
     }else{
         sum=sum-Sgc-S20+7;
-        in_site[ini].score=sum;
-        in_site[ini].Sspe=rs.dou[1];
-        in_site[ini].Seff=rs.dou[2];
+        in_site[ini].Sspe=rs.dou[1]=r1*(sum);
+        in_site[ini].Seff=rs.dou[2]=r2*(100-Sgc-S20);
+        in_site[ini].score=rs.dou[0]=in_site[ini].Sspe+in_site[ini].Seff;
         in_site[ini].count=in_site[ini].ot.size();
         (*pini)++;
-        rs.dou[0]=sum;
     }
+
+    in_site[ini].otj=dc_put(1,ini);
+
     return rs;
 }
